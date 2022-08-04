e6e50a1f90a91628ad533c49a982f37767ff07e3
Lunadoxx <williamdimmickyt@gmail.com>
2022-08-03 20:26:11 -0500

    bruh

--- g/LunarClicker/script.js
+++ g/LunarClicker/script.js
@@ -1,5 +1,10 @@
 let num = 0;
+let jsonString = localStorage.getItem("num");
+let save = JSON.parse(jsonString);
+num = num + save - 1;
 function addOne() {
     num++;
     document.getElementById("number").innerHTML = num;
+    localStorage.setItem("num", JSON.stringify(num));
 }
+document.getElementById("number").innerHTML = save;
